b3cc7c8f650b3d9074cf6d89f0990e5f5ca1f14b
kumami-tech <sk924731@gmail.com>
2020-07-14 18:03:36 +0900

    update posts controller

--- app/controllers/posts_c_controller.rb
+++ app/controllers/posts_c_controller.rb
@@ -1,2 +1,9 @@
 class PostsCController < ApplicationController
+  def index
+    @posts = PostC.all
+  end
+
+  def new
+    @post = PostC.new
+  end
 end
--- app/controllers/posts_g_controller.rb
+++ app/controllers/posts_g_controller.rb
@@ -1,2 +1,22 @@
 class PostsGController < ApplicationController
+  before_action :set_tweet, only: [:edit]
+  before_action :move_to_index, except: [:index]
+
+  def index
+    @posts = PostG.includes(:user).order("created_at DESC")
+  end
+
+  def new
+    @post = PostG.new
+  end
+
+
+  private
+  def set_post
+    @post = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
