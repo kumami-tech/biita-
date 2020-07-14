af0ba5c9136720074bf872edd8d64dcd20a12afb
kumami-tech <sk924731@gmail.com>
2020-07-14 18:01:11 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root "home#index"
   resources :users, only: [:index, :edit, :update]
+  resources :posts_g
+  resources :posts_c
 end
