e049131e1d60a567733cac7ef7e4bae5b2cab71c
kumami-tech <sk924731@gmail.com>
2020-07-16 12:46:27 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,24 +2,37 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.name
+    @users = User.all
+    @user = User.new
   end
 
   def show
     @posts = PostG.where(user_id: current_user.id)
+    @user = User.find(params[:id])
     # user = User.find(params[:id])
     @name = current_user.name
     # posts_g = PostG.all
     @posts_g = current_user.post_gs
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :create
+    end
+  end
+
   def edit
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
     if current_user.update(user_params)
-      redirect_to root_path
+      # binding.pry
+      redirect_to request.referer
+      # redirect_to root_path
     else
       render :edit
     end
@@ -28,6 +41,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :name, :email)
+    params.require(:user).permit(:id, :name, :email, :profile_image, :profile)
   end
 end
