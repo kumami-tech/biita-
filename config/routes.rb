92f51d6a7f0fa77f5976468cbac2e639958fa88f
kumami-tech <sk924731@gmail.com>
2020-07-16 18:59:32 +0900

    g messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'messages/index'
+  get 'messages/create'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
