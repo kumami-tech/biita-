ecc39435ad89aa6ef9205570b3af31af5d8d9b78
kumami-tech <sk924731@gmail.com>
2020-07-17 19:14:33 +0900

    update messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,25 @@
 class MessagesController < ApplicationController
-  def index
+  # before_action :set_group
+
+  # def create
+  #   @message = Message.new
+  #   redirect_to message_path(@message.user.id)
+  # end
+
+  def show
+    # binding.pry
+    @user = User.find(params[:id])
+    # @message = Message.new
+    # @messages = @group.messages.includes(:user)
   end
 
-  def create
+  # def set_group
+  #   @group = Group.find(params[:group_id])
+  # end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
   end
 end
