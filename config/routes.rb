587b4ccbe6be9d4cdf2ce4c7a6bf38cd28bc6bf5
kumami-tech <sk924731@gmail.com>
2020-07-19 20:17:26 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,22 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+  get 'reviews/show'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+
   root "home#index"
   resources :users
-  # resources :users, only: [:show, :edit, :update]
   resources :posts_g
   resources :posts_c
-  resources :groups
-  resources :messages
+  resources :reviews, only: [:create, :show]
+
+  resources :groups, only: [:index, :show] do
+    resources :messages, only: [:index, :create]
+  end
+
   get '/posts_g/:id/destroy', to: 'posts_g#destroy'
   get '/posts_c/:id/destroy', to: 'posts_c#destroy'
+  get '/groups/:id/create', to: 'groups#create'
 end
