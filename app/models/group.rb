416b23f88a76e3b4b3479438ade10a3c78626d2c
kumami-tech <sk924731@gmail.com>
2020-08-18 00:04:37 +0900

    create side_bar

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,16 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   has_many :messages
+
+  def show_last_message
+    if (last_message = messages.first).present?
+      if last_message.text?
+        last_message.text
+      else
+        '画像が投稿されています'
+      end
+    else
+      'メッセージがありません'
+    end
+  end
 end
