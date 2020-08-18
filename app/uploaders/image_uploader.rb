06032af214f7f6e84659a8f6e44363b020489786
kumami-tech <sk924731@gmail.com>
2020-08-18 09:33:44 +0900

    changed refile to carrierwave

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -14,12 +14,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url(*args)
-    # For Rails 3.1+ asset pipeline compatibility:
-    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  # def default_url(*args)
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   
-    "/images/fallback/" + [version_name, "no-image.jpg"].compact.join('_')
-  end
+  #   "no-image.jpg"
+  #   # "/images/fallback/" + [version_name, "no-image.jpg"].compact.join('_')
+  # end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
@@ -30,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-    process resize_to_fit: [300, 600]
+    process resize_to_fit: [680, 400]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
