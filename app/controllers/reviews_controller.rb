c436cdcb3625bf9b07133592d375907e715b3c82
kumami-tech <sk924731@gmail.com>
2020-08-28 20:42:52 +0900

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,26 +3,29 @@ class ReviewsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
+    @reviews = @user.reviews.includes(:user)
   end
   
-  def create
+  def new
     @user = User.find(params[:user_id])
-    @review = @user.reviews.new(review_params)
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
     if @review.save
-      respond_to do |format|
-        # format.html { redirect_to "/users/#{@user.id}/reviews" }
-        format.json
-      end
+      flash[:notice] = 'レビューを投稿しました。'
+      redirect_to user_reviews_path
     else
-      @reviews = @user.reviews.includes(:user)
-      render :index
+      flash.now[:alert] = 'レビューを投稿できませんでした。'
+      render :new
     end
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content).merge(user_id: current_user.id)
+    params.require(:review).permit(:content, :score, :reviewee_id).merge(reviewer_id: current_user.id)
   end
 
 end
