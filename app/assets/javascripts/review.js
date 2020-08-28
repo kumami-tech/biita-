9d58429a3f488cb3509346d17f8e9136771acfbd
kumami-tech <sk924731@gmail.com>
2020-08-28 23:28:33 +0900

    updated function of stars

--- app/assets/javascripts/review.js
+++ app/assets/javascripts/review.js
@@ -1,11 +1,41 @@
-const stars = $(".Star");
-const ratingValue = $("#rating-value");
-const ratingValueDisplay = $("#rating-value-display");
-
 $(function(){
-	$('.Star').hover(function(){
-		let index = $('.Star').index($(this));
-		for(let j=0; j<=index; j++){
-			stars.eq(j).attr('src', '/assets/star-on.png').addClass('On');
+
+	const stars = $('.Star');
+	let index;
+
+	$('.Star').click(function(){
+		index = $('.Star').index($(this));
+		for(let j=0; j<stars.length; j++){
+			stars.eq(j).css('color', 'silver')
 		}
+		for(let j=0; j<=index; j++){
+			stars.eq(j).css('color', 'orange')
+		}
+		$('#rating-value').val(index + 1);
+		$('#rating-value-display').text(index + 1);
+	})
+
+	$('.Star').hover(
+		function(){
+			let i = $('.Star').index($(this));
+			for(let j=0; j<stars.length; j++){
+				stars.eq(j).css('color', 'silver')
+			}
+			for(let j=0; j<=i; j++){
+				stars.eq(j).css('color', 'orange')
+			}
+			$('#rating-value-display').text(i + 1);
+		},
+		function(){
+			for(let j=0; j<stars.length; j++){
+				stars.eq(j).css('color', 'silver')
+				$('#rating-value-display').text(0);
+			}
+			for(let j=0; j<=index; j++){
+				stars.eq(j).css('color', 'orange')
+				$('#rating-value-display').text(index + 1);
+			}
+		}
+	)
+
 });
