78118801de0d0573ea253599e4f1cc0b95df45c7
kumami-tech <sk924731@gmail.com>
2020-09-01 19:01:19 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,7 @@ class UsersController < ApplicationController
     reviews = Review.where(reviewee_id: @user.id)
     @count = reviews.count
 
-    group_ids = @user.groups.pluck(:id)
-    group_users = GroupUser.where(group_id: group_ids)
-    group_user = group_users.where(user_id: current_user.id)
-    @group_id = group_user.pluck(:group_id).first
+    @group_id = (@user.groups & current_user.groups).first.id
   end
 
   def edit
