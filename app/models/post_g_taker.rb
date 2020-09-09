465b9a7f7682f09af176e5f6675ad72231ee2f64
kumami-tech <sk924731@gmail.com>
2020-09-09 21:13:16 +0900

    Update post_g_taker.rb

--- app/models/post_g_taker.rb
+++ app/models/post_g_taker.rb
@@ -1,4 +1,4 @@
 class PostGTaker < ApplicationRecord
   belongs_to :taking_post_g, class_name: 'PostG', foreign_key: 'post_g_id'
-  belongs_to :taker, class_name: 'User', foreign_key: 'post_g_id'
+  belongs_to :taker, class_name: 'User', foreign_key: 'taker_id'
 end
