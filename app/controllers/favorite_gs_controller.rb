4a257c3fb61c0e0225d67a8fda66e0b260fde49a
kumami-tech <sk924731@gmail.com>
2020-09-11 21:54:20 +0900

    update favorite function

--- app/controllers/favorite_gs_controller.rb
+++ app/controllers/favorite_gs_controller.rb
@@ -1,11 +1,11 @@
 class FavoriteGsController < ApplicationController
   def create
-    FavoriteG.create!(user_id: current_user.id, post_g_id: params[:post_g_id])
+    FavoriteG.create!(user_id: current_user.id, post_g_id: params[:posts_g_id])
     redirect_to posts_g_path(params[:posts_g_id])
   end
 
   def destroy
-    favorite = FavoriteG.find_by(post_g_id: params[:post_g_id], user_id: current_user.id)
+    favorite = FavoriteG.find_by(post_g_id: params[:posts_g_id], user_id: current_user.id)
     favorite.destroy
     redirect_to posts_g_path(params[:posts_g_id])
   end
--- db/migrate/20200911045256_create_favorite_gs.rb
+++ db/migrate/20200911045256_create_favorite_gs.rb
@@ -2,7 +2,7 @@ class CreateFavoriteGs < ActiveRecord::Migration[6.0]
   def change
     create_table :favorite_gs do |t|
       t.integer :user_id
-      t.integer :post_g_id
+      t.integer :posts_g_id
       t.timestamps
     end
   end
