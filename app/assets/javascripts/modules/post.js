fdf2f87a9315262485c874ff643cf4c4a32b3044
kumami-tech <sk924731@gmail.com>
2020-09-12 16:06:10 +0900

    favorite function js

--- app/assets/javascripts/modules/post.js
+++ app/assets/javascripts/modules/post.js
@@ -3,3 +3,24 @@
 //     $('.Post_bookmark--Icon').toggle();
 //   })
 // })
+
+// $(function(){
+//   $('.Post_bookmark--Icon').click(function() {
+//     if ($('.Post_bookmark--Icon').hasClass('far')) {
+//       $('.Post_bookmark--Icon').removeClass('far').addClass('fas');
+//       let url = $('.Post_bookmark--Icon').parent().attr('href') ;
+//       console.log(url)
+//       $.ajax({
+//         url: url,
+//         type: 'POST',
+//         data: formData,  
+//         dataType: 'json',
+//       })
+//     } else {
+//       $('.Post_bookmark--Icon').removeClass('fas').addClass('far')
+//     }
+    
+//   })
+// })
+
+
--- app/controllers/favorite_gs_controller.rb
+++ app/controllers/favorite_gs_controller.rb
@@ -1,12 +1,23 @@
 class FavoriteGsController < ApplicationController
-  def create
-    FavoriteG.create!(user_id: current_user.id, post_g_id: params[:posts_g_id])
-    redirect_to posts_g_path(params[:posts_g_id])
+  before_action :set
+
+  def create!
+    binding.pry
+    FavoriteG.create(user_id: current_user.id, post_g_id: params[:posts_g_id])
   end
 
   def destroy
+    binding.pry
     favorite = FavoriteG.find_by(post_g_id: params[:posts_g_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to posts_g_path(params[:posts_g_id])
   end
+
+  private
+  def set
+    @post = PostG.find(params[:posts_g_id])
+    @user = @post.giver
+    @favorite = @post.favorite_gs.where(user_id: current_user.id)
+  end
+
 end
+
