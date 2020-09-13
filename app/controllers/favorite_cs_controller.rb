52795ded466e044ef136473baa446591a7198cfd
kumami-tech <sk924731@gmail.com>
2020-09-13 15:49:25 +0900

    edit posts#show for signed-out user

--- app/controllers/favorite_cs_controller.rb
+++ app/controllers/favorite_cs_controller.rb
@@ -1,4 +1,5 @@
 class FavoriteCsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set
 
   def create
--- app/controllers/posts_g_controller.rb
+++ app/controllers/posts_g_controller.rb
@@ -19,14 +19,16 @@ class PostsGController < ApplicationController
     reviews = Review.where(reviewee_id: @user.id)
     @count = reviews.count
 
-    group_ids = @user.groups.pluck(:id)
-    group_users = GroupUser.where(group_id: group_ids)
-    group_user = group_users.where(user_id: current_user.id)
-    @group_id = group_user.pluck(:group_id).first
+    if user_signed_in?
+      group_ids = @user.groups.pluck(:id)
+      group_users = GroupUser.where(group_id: group_ids)
+      group_user = group_users.where(user_id: current_user.id)
+      @group_id = group_user.pluck(:group_id).first
+
+      @favorite = @post.favorite_gs.where(user_id: current_user.id)
+    end
 
     @takers = @post.takers
-
-    @favorite = @post.favorite_gs.where(user_id: current_user.id)
   end
 
   def new
