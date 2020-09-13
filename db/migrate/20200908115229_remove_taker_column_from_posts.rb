95497483cde6de2c357e09ab15f4f5a30e8ad3b6
kumami-tech <sk924731@gmail.com>
2020-09-13 18:05:07 +0900

    Delete 20200908115229_remove_taker_column_from_posts.rb

--- db/migrate/20200908115229_remove_taker_column_from_posts.rb
+++
@@ -1,8 +0,0 @@
-class RemoveTakerColumnFromPosts < ActiveRecord::Migration[6.0]
-  def change
-    remove_foreign_key :post_gs, :users
-    remove_reference :post_gs, :taker
-    remove_foreign_key :post_cs, :users
-    remove_reference :post_cs, :taker
-  end
-end
