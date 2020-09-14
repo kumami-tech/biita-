18d52d6fea41b26ceb06e828a5451227a339ece3
kumami-tech <sk924731@gmail.com>
2020-09-14 19:08:07 +0900

    create notifications controller

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,13 @@
+class NotificationsController < ApplicationController
+  def index
+    @notifications = current_user.passive_notifications
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
+
+  def destroy_all
+    @notifications = current_user.passive_notifications.destroy_all
+    redirect_to users_notifications_path
+  end
+end
