7046f624277965771923e3ec9c6e369fd62c45c0
kumami-tech <sk924731@gmail.com>
2020-09-16 18:05:06 +0900

    レビュー通知機能の実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,7 @@ class Review < ApplicationRecord
   validates :position, presence: true
   belongs_to :reviewer, class_name: 'User', foreign_key: 'reviewer_id'
   belongs_to :reviewee, class_name: 'User', foreign_key: 'reviewee_id'
+  has_many :notifications, dependent: :destroy
 
   def display_position
     if self.position == "guest"
@@ -13,4 +14,14 @@ class Review < ApplicationRecord
     end
   end
 
+  def create_notification_review!(current_user, user)
+    notification = current_user.active_notifications.new(
+      visited_id: user.id,
+      action: 'review'
+    )
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w[message.js]
 Rails.application.config.assets.precompile += %w[autoReload.js]
 Rails.application.config.assets.precompile += %w[map.js]
