92e8b802690de000723d0c54b49c0f221da5de18
kumami-tech <sk924731@gmail.com>
2020-09-17 18:42:24 +0900

    [fix]フォロー：Userモデル外部のキーを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,9 +45,9 @@ class User < ApplicationRecord
   has_many :passive_notifications, class_name: "Notification", foreign_key: "visited_id", dependent: :destroy
 
   # フォロー
-  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship",  dependent: :destroy
+  has_many :following_relationships, foreign_key: "following_id", class_name: "Relationship",  dependent: :destroy
   has_many :followings, through: :following_relationships
-  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :follower_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :follower_relationships
 
   def following?(other_user)
@@ -55,10 +55,10 @@ class User < ApplicationRecord
   end
 
   def follow(other_user)
-    self.following_relationships.create(following_id: other_user.id)
+    self.following_relationships.create(follower_id: other_user.id)
   end
 
   def unfollow(other_user)
-    self.following_relationships.find_by(following_id: other_user.id).destroy
+    self.following_relationships.find_by(follower_id: other_user.id).destroy
   end
 end
