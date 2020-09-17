a39b675359203a4e86a7e53a15f5b2a7046e2a06
kumami-tech <sk924731@gmail.com>
2020-09-17 16:00:07 +0900

    [add]フォロー：Userモデルにメソッドを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,4 +49,16 @@ class User < ApplicationRecord
   has_many :followings, through: :following_relationships
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :follower_relationships
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
+  def follow(other_user)
+    self.following_relationships.create(following_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    self.following_relationships.find_by(following_id: other_user.id).destroy
+  end
 end
