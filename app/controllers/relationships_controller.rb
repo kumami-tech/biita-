2f2131e367870245d8f4624ee1e74a58e4cd4821
kumami-tech <sk924731@gmail.com>
2020-09-18 14:24:39 +0900

    [update]フォロー：非同期化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,20 +3,18 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.follow(@user)
-    redirect_back(fallback_location: root_url)
-    # respond_to do |format|
-    #   format.html {redirect_back(fallback_location: root_url)}
-    #   format.js
-    # end
+    respond_to do |format|
+      format.html {redirect_back(fallback_location: root_url)}
+      format.js
+    end
   end
 
   def destroy
     current_user.unfollow(@user)
-    redirect_back(fallback_location: root_url)
-    # respond_to do |format|
-    #   format.html {redirect_back(fallback_location: root_url)}
-    #   format.js
-    # end
+    respond_to do |format|
+      format.html {redirect_back(fallback_location: root_url)}
+      format.js
+    end
   end
 
   private
