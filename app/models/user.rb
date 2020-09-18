65414bb7cff8b328767516644a16e6077ea135dd
kumami-tech <sk924731@gmail.com>
2020-09-18 14:07:57 +0900

    [fix]フォロー：アソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -46,10 +46,10 @@ class User < ApplicationRecord
 
   # フォロー
   has_many :following_relationships, foreign_key: "following_id", class_name: "Relationship",  dependent: :destroy
-  has_many :followings, through: :following_relationships
+  has_many :followings, through: :following_relationships, source: :follower
   has_many :follower_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
-  has_many :followers, through: :follower_relationships
-
+  has_many :followers, through: :follower_relationships, source: :following
+ 
   def following?(other_user)
     self.followings.include?(other_user)
   end
