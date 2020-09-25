39d156573270003af01cfff9329c7556962e00d6
kumami-tech <sk924731@gmail.com>
2020-09-25 11:08:41 +0900

    [add]レスポンシブ：ヘッダー

--- app/assets/javascripts/modules/header.js
+++ app/assets/javascripts/modules/header.js
@@ -1,9 +1,31 @@
 $(function() {
+  $(document).click(function() {$('.Menu').hide();});
+  $('.Header_btn').click(function() {event.stopPropagation();});
+
   $("#Post_btn").click(function() {
     $("#Post_menu").toggle();
   })
-
   $("#Header_user_image").click(function() {
     $("#User_menu").toggle();
+  })
+
+  // レスポンシブ
+
+  $("#Post_btn_s").click(function() {
+    $("#Post_menu_s").toggle();
+  })
+
+  $('#Post_btn_s').click(function() {event.stopPropagation();});
+
+  $("#Search_btn").click(function() {
+    $("#Search_menu").toggle();
+  })
+
+  $("#Menu_btn").click(function() {
+    $("#Menu_logout").toggle();
+  })
+  
+  $("#Header_user_image_s").click(function() {
+    $("#Menu_login").toggle();
   })
 });
