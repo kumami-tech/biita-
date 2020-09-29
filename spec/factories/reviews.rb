805da3e29f8a18c8b158bcc7e4308697f447c740
kumami-tech <sk924731@gmail.com>
2020-09-29 21:51:34 +0900

    [add]テスト：Reviewモデル

---
+++ spec/factories/reviews.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+
+  factory :review do
+    association :reviewee, factory: :user
+    association :reviewer, factory: :user
+    content                  {Faker::Lorem.sentence}
+    score                    {"5"}
+    position                 {"guest"}
+  end
+
+end
