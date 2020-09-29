dae909f9023055c1c2aa5ee0563ab523b8e855e4
kumami-tech <sk924731@gmail.com>
2020-09-29 19:02:59 +0900

    [add]テスト：Postモデル

---
+++ spec/factories/posts.rb
@@ -0,0 +1,22 @@
+FactoryBot.define do
+  factory :post_g do
+    association :giver, factory: :user
+    title        {"トロントで観光案内"}
+    tag_list     {"#トロント"}
+    region       {"カナダ・トロント"}
+    datetime     {"9月24日"}
+    charge       {"10000円"}
+    payment      {"現地で手渡し"}
+    content      {"トロントの観光案内"}
+  end
+
+  factory :post_c do
+    association :giver, factory: :user
+    title        {"トロントで観光案内"}
+    region       {"カナダ・トロント"}
+    datetime     {"9月24日"}
+    charge       {"10000円"}
+    payment      {"現地で手渡し"}
+    content      {"トロントの観光案内"}
+  end
+end
