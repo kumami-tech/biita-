9628328bdaf1b78db271d330fea5f7a752b8e3cc
kumami-tech <sk924731@gmail.com>
2020-10-01 21:05:15 +0900

    [add]テスト：homeコントローラー

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+describe HomeController do
+  describe 'GET #index' do
+    let(:user) { create(:user) }
+
+    context "ユーザーがログインしている場合" do
+      before do
+        login user
+        get :index
+      end
+      
+      it "トップページに遷移すること" do
+        expect(response).to render_template :index
+      end
+      
+      it "HTTPのレスポンスが200であること" do
+        expect(response).to have_http_status "200"
+      end
+    end
+
+    context "ユーザーがログインしていない場合" do
+      before do
+        get :index
+      end
+      
+      it "トップページに遷移すること" do
+        expect(response).to render_template :index
+      end
+
+      it "HTTPのレスポンスが200であること" do
+        expect(response).to have_http_status "200"
+      end
+    end
+  end
+end
