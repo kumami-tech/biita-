2c30fa391bb6b00feaf5ea0e7e7fc205e5356fae
kumami-tech <sk924731@gmail.com>
2020-10-01 15:44:30 +0900

    [update]テスト：Postsコントローラーのcreateアクション

--- spec/controllers/posts_g_controller_spec.rb
+++ spec/controllers/posts_g_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe PostsGController do
   let(:giver) { create(:user) }
   let(:current_user) { create(:user) }
-  let(:post) { create(:post_g, giver: giver) }
+  let(:post_g) { create(:post_g, giver: giver) }
   let(:reviews) { create_list(:review, 3, reviewee: giver) }
   # let!(:favorite) { create(:favorite_g, post_g: post, user: current_user) }
 
@@ -29,7 +29,7 @@ describe PostsGController do
 
   describe 'GET #show' do
     before do
-      get :show, params: {id: post.id}
+      get :show, params: {id: post_g}
     end
 
     it "投稿詳細ページに遷移すること" do
@@ -41,7 +41,7 @@ describe PostsGController do
     end
 
     it "適切にインスタンス変数(@post)が取り出されること" do
-      expect(assigns(:post)).to eq post
+      expect(assigns(:post)).to eq post_g
     end
 
     it "適切にインスタンス変数(@user)が取り出されること" do
@@ -57,8 +57,8 @@ describe PostsGController do
     # end
 
     it "適切にインスタンス変数(@takers)が取り出されること" do
-      get :show, params: { id: post }
-      expect(assigns(:takers)).to eq post.takers
+      get :show, params: { id: post_g }
+      expect(assigns(:takers)).to eq post_g.takers
     end
   end
 
@@ -101,7 +101,7 @@ describe PostsGController do
     context "ユーザーがログインしている場合" do
       before do
         login giver
-        get :edit, params: {id: post}
+        get :edit, params: {id: post_g}
       end
 
       it "投稿編集ページに遷移すること" do
@@ -113,13 +113,13 @@ describe PostsGController do
       end
 
       it "適切にインスタンス変数(@post)が取り出されること" do
-        expect(assigns(:post)).to eq post
+        expect(assigns(:post)).to eq post_g
       end
     end
 
     context "ユーザーがログインしていない場合" do
       before do
-        get :edit, params: {id: post}
+        get :edit, params: {id: post_g}
       end
 
       it "ログイン画面にリダイレクトされること" do
@@ -130,6 +130,72 @@ describe PostsGController do
         expect(response).to have_http_status "302"
       end
     end
+  end
+
+  describe '#create' do
+    let(:params) { { giver_id: current_user.id, post_g: attributes_for(:post_g) } }
+
+    context "ユーザーがログインしている場合" do
+      before do
+        login giver
+      end
+
+      context "投稿が保存できる場合" do
+
+        subject {
+          post :create,
+          params: params
+        }
+
+        it "投稿が保存されること" do
+          expect{ subject }.to change(PostG, :count).by(1)
+        end
+
+        # it "投稿詳細ページにリダイレクトされること" do
+        #   subject
+        #   expect(response).to redirect_to posts_g_path(params[:post_g])
+        # end
+      end
+
+      context "投稿が保存できない場合" do
+        let(:invalid_params) { { giver_id: current_user.id, post_g: attributes_for(:post_g, title: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it "投稿が保存されないこと" do
+          expect{ subject }.not_to change(PostG, :count)
+        end
+
+        it "新規投稿ページにリダイレクトされること" do
+          subject
+          expect(response).to redirect_to new_posts_g_path
+        end
+      end
+    end
+
+
+    context "ユーザーがログインしていない場合" do
+      subject {
+        post :create,
+        params: params
+      }
+
+      it "ログイン画面にリダイレクトされること" do
+        subject
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it "HTTPのレスポンスが302であること" do
+        subject
+        expect(response).to have_http_status "302"
+      end
+    end
+
+
+
   end
 
 end
