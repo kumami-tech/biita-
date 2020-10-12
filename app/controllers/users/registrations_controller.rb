434a580772c34cf4ffb0f5c5fd2f41f675b256e5
kumami-tech <sk924731@gmail.com>
2020-10-12 14:06:10 +0900

    [update]rubocop: 不要な文字の削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,6 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: [:update, :destroy]
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
 
   def edit
     @user = User.find(current_user.id)
@@ -15,7 +13,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     end
   end
 
-  private
+  # private
 
   # GET /resource/sign_up
   # def new
