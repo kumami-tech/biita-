c0e1912e6b0f7fe783cafa23dcc874353ed47c1d
kumami-tech <sk924731@gmail.com>
2020-10-12 16:07:18 +0900

    [update]rubocop: 文字列の配列の書き方を"%w"に変更

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,6 +7,6 @@ class Notification < ApplicationRecord
   belongs_to :review, optional: true
   belongs_to :visitor, class_name: 'User', foreign_key: 'visitor_id'
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id'
-  ACTION_VALUES = ["message", "review", "apply_g", "apply_c", "cancel_g", "cancel_c", "favorite_g", "favorite_c", "follow"].freeze
+  ACTION_VALUES = %w[message review apply_g apply_c cancel_g cancel_c favorite_g favorite_c follow].freeze
   validates :action, presence: true, inclusion: { in: ACTION_VALUES }
 end
