a3b96753a26a24fb459c766d3ab83de18421f593
kumami-tech <sk924731@gmail.com>
2020-10-12 12:06:02 +0900

    [update]rubocop: image_uploaderの修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -12,24 +12,24 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+    # Provide a default URL as a default if there hasn't been a file uploaded:
+    # def default_url(*args)
+    #   # For Rails 3.1+ asset pipeline compatibility:
+    #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   
-  #   "no-image.jpg"
-  #   # "/images/fallback/" + [version_name, "no-image.jpg"].compact.join('_')
-  # end
+    #   "no-image.jpg"
+    #   # "/images/fallback/" + [version_name, "no-image.jpg"].compact.join('_')
+    # end
 
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+    # Process files as they are uploaded:
+    # process scale: [200, 300]
+    #
+    # def scale(width, height)
+    #   # do something
+    # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
+    # Create different versions of your uploaded files:
+    # version :thumb do
     process resize_to_fit: [680, 400]
   # end
 
