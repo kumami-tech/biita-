99b4477a2062332c7358a9caaeb94d8314766e45
kumami-tech <sk924731@gmail.com>
2020-10-24 12:45:53 +0900

    [update]地図表示のコードを修正

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,19 +1,20 @@
-$(function(){
+window.addEventListener("load", function() {
+  const inputAddress = document.getElementById('address').textContent;
+  const target = document.getElementById('map');
   geocoder = new google.maps.Geocoder()
-  let Address = document.getElementById('address').textContent;
 
-  geocoder.geocode( { 'address': Address}, function(results, status) {
+  geocoder.geocode( { address: inputAddress}, function(results, status) {
     if (status == 'OK') {
-      map = new google.maps.Map(document.getElementById('map'), {
+      const map = new google.maps.Map(target, {
+        center: results[0].geometry.location,
         zoom: 13
       });
-      map.setCenter(results[0].geometry.location);
-      const marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
+      new google.maps.Marker({
+        map: map,
+        position: results[0].geometry.location,
       });
     } else {
-      document.getElementById('map').style.height = '14px'
+      document.getElementById('map').style.height = '20px'
       document.getElementById('map').innerHTML = '該当する場所が見つかりませんでした。'
     }
   });   
