59aec22b67fb60e01b052080ce76f076701cf060
kumami-tech <sk924731@gmail.com>
2020-10-31 16:12:59 +0900

    [update]header.jsを素のJavaScriptに修正

--- app/assets/javascripts/modules/header.js
+++ app/assets/javascripts/modules/header.js
@@ -1,15 +1,26 @@
-$(function() {
-  $(document).click(function() {$('.Menu').hide();});
-  $('.Header_btn').click(function() {event.stopPropagation();});
+document.addEventListener('DOMContentLoaded', () => {
+  const menus = document.querySelectorAll('.Menu')
+  const headerBtns = document.querySelectorAll('.Header_btn')
+  const menuBtns = document.querySelectorAll('.Menu_btn')
 
-  $("#Post_btn").click(function() {
-    $('#User_menu').hide();
-    $("#Post_menu").toggle();
-  })
-  $("#Header_user_image").click(function() {
-    $('#Post_menu').hide();
-    $("#User_menu").toggle();
-  })
+  for (let i = 0; i < menus.length; i++) {
+    document.addEventListener('click', () => {
+      menus[i].style.display = 'none'
+    })
+  }
+  for (let i = 0; i < headerBtns.length; i++) {
+    headerBtns[i].addEventListener('click', (e) => {
+      e.stopPropagation();
+    })
+  }
+  for (let i = 0; i < menuBtns.length; i++) {
+    menuBtns[i].addEventListener('click', () => {
+      for (let j = 0; j < menuBtns.length; j++) {
+        menus[j].style.display = 'none'
+      }
+      menus[i].style.display = 'block'
+    })
+  }
 
   // レスポンシブ
 
