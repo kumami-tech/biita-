dea3c42e4da0668796baba6605fb2e0b4855bd0c
kumami-tech <sk924731@gmail.com>
2020-10-31 15:19:45 +0900

    [update]マイページタブのリファクタリング

--- app/assets/javascripts/modules/mypage.js
+++ app/assets/javascripts/modules/mypage.js
@@ -18,25 +18,23 @@ document.addEventListener('DOMContentLoaded', () => {
 // 上のタブ
 document.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.User_page_tab');
-  const givingPosts = document.querySelectorAll('.User_giving_posts');
-  const takenPosts = document.querySelectorAll('.User_taken_posts');
-  const takingPosts = document.querySelectorAll('.User_taking_posts');
-  const favoritePosts = document.querySelectorAll('.User_favorite_posts');
+  const allContents = document.querySelectorAll('[data-content]');
 
   for (let i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener('click', e => {
+
       for (let j = 0; j < tabs.length; j++) {
         tabs[j].classList.remove('Active2')
-        givingPosts[j].classList.remove('Show2')
-        takenPosts[j].classList.remove('Show2')
-        takingPosts[j].classList.remove('Show2')
-        favoritePosts[j].classList.remove('Show2')
+        for (let k = 0; k < allContents.length; k++) {
+          allContents[k].classList.remove('Show2')
+        }
       }
       tabs[i].classList.add('Active2')
-      givingPosts[i].classList.add('Show2')
-      takenPosts[i].classList.add('Show2')
-      takingPosts[i].classList.add('Show2')
-      favoritePosts[i].classList.add('Show2')
+
+      for (let k = 0; k < allContents.length / tabs.length; k++) {
+        const contents = document.querySelectorAll('[data-content="' + k + '"]');
+        contents[i].classList.add('Show2')
+      }
     });
   }
 });
