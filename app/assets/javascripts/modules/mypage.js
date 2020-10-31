dfab424087a6439ab273d0e78239b787b117af27
kumami-tech <sk924731@gmail.com>
2020-10-31 15:12:31 +0900

    [update]mypage.jsを素のJavaScriptに書き換え

--- app/assets/javascripts/modules/mypage.js
+++ app/assets/javascripts/modules/mypage.js
@@ -1,26 +1,42 @@
+// 左のボタン
 document.addEventListener('DOMContentLoaded', () => {
-  let btns = $(".Post_type_btn");
+  const btns = document.querySelectorAll('.Post_type_btn');
+  const contents = document.querySelectorAll('.Mypage_lower__Content');
 
-  function btnSwitch() {
-    $(".Active1").removeClass("Active1");
-    $(this).addClass("Active1");
-    const index = btns.index(this);
-    $(".Mypage_lower__Content").removeClass("Show1").eq(index).addClass("Show1");
+  for (let i = 0; i < btns.length; i++) {
+    btns[i].addEventListener('click', e => {
+      for (let j = 0; j < btns.length; j++) {
+        btns[j].classList.remove('Active1')
+        contents[j].classList.remove('Show1')
+      }
+      btns[i].classList.add('Active1');
+      contents[i].classList.add('Show1');
+    })
   }
-  btns.click(btnSwitch);
 });
 
+// 上のタブ
 document.addEventListener('DOMContentLoaded', () => {
-  let tabs = $(".User_page_tab");
+  const tabs = document.querySelectorAll('.User_page_tab');
+  const givingPosts = document.querySelectorAll('.User_giving_posts');
+  const takenPosts = document.querySelectorAll('.User_taken_posts');
+  const takingPosts = document.querySelectorAll('.User_taking_posts');
+  const favoritePosts = document.querySelectorAll('.User_favorite_posts');
 
-  function tabSwitch() {
-    $(".Active2").removeClass("Active2");
-    $(this).addClass("Active2");
-    const index = tabs.index(this);
-    $(".User_giving_posts").removeClass("Show2").eq(index).addClass("Show2");
-    $(".User_taken_posts").removeClass("Show2").eq(index).addClass("Show2");
-    $(".User_taking_posts").removeClass("Show2").eq(index).addClass("Show2");
-    $(".User_favorite_posts").removeClass("Show2").eq(index).addClass("Show2");
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener('click', e => {
+      for (let j = 0; j < tabs.length; j++) {
+        tabs[j].classList.remove('Active2')
+        givingPosts[j].classList.remove('Show2')
+        takenPosts[j].classList.remove('Show2')
+        takingPosts[j].classList.remove('Show2')
+        favoritePosts[j].classList.remove('Show2')
+      }
+      tabs[i].classList.add('Active2')
+      givingPosts[i].classList.add('Show2')
+      takenPosts[i].classList.add('Show2')
+      takingPosts[i].classList.add('Show2')
+      favoritePosts[i].classList.add('Show2')
+    });
   }
-  tabs.click(tabSwitch);
 });
