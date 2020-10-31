c7a0c419b6f7879f102e3ccf82928773fc366f5b
kumami-tech <sk924731@gmail.com>
2020-10-31 16:23:55 +0900

    [update]ヘッダーボタンのリファクタリング

--- app/assets/javascripts/modules/header.js
+++ app/assets/javascripts/modules/header.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  
   const menus = document.querySelectorAll('.Menu')
   const headerBtns = document.querySelectorAll('.Header_btn')
   const menuBtns = document.querySelectorAll('.Menu_btn')
@@ -8,11 +9,13 @@ document.addEventListener('DOMContentLoaded', () => {
       menus[i].style.display = 'none'
     })
   }
+
   for (let i = 0; i < headerBtns.length; i++) {
     headerBtns[i].addEventListener('click', (e) => {
       e.stopPropagation();
     })
   }
+
   for (let i = 0; i < menuBtns.length; i++) {
     menuBtns[i].addEventListener('click', () => {
       for (let j = 0; j < menuBtns.length; j++) {
@@ -22,31 +25,4 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
 
-  // レスポンシブ
-
-  $("#Post_btn_s").click(function() {
-    $('#Search_menu').hide();
-    $('#Menu_logout').hide();
-    $('#Menu_login').hide();
-    $("#Post_menu_s").toggle();
-  })
-
-  $("#Search_btn").click(function() {
-    $("#Post_menu_s").hide();
-    $('#Menu_logout').hide();
-    $('#Menu_login').hide();
-    $("#Search_menu").toggle();
-  })
-
-  $("#Menu_btn").click(function() {
-    $("#Post_menu_s").hide();
-    $('#Search_menu').hide();
-    $("#Menu_logout").toggle();
-  })
-  
-  $("#Header_user_image_s").click(function() {
-    $("#Post_menu_s").hide();
-    $('#Search_menu').hide();
-    $("#Menu_login").toggle();
-  })
 });
